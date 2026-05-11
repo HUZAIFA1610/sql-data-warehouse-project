49058f55a864f588a74a766fd402a7f2cf64f0ab
Mohammad Abu Huzaifa <147640851+HUZAIFA1610@users.noreply.github.com>
2026-05-12 04:05:27 +0600

    Create quality_checks_silver.sql

---
+++ tests/quality_checks_silver.sql
@@ -0,0 +1,138 @@
+/*
+=============================================================
+Quality Checks:
+-------------------------------------------------------------
+Script Purpose:
+  This script performs various quality checks for data consistency, accuracy, and standardization across the 'silver' schemas. It includes checks for:
+  - Null or duplicate primary keys.
+  - Unwanted spaces in string fields.
+  - Data standardization and consistency.
+  - Invalid date ranges and orders.
+  - Data consistency between related fields.
+
+Usage Notes:
+  - Run these checks after data loading silver layer.
+  - Investigate and resolve any discrepancies found during the checks.
+
+=============================================================
+*/
+
+-- Test the quality of crm_cust_info
+-- Check for Null & Duplicates in Primary Key
+-- Expectation: No results
+SELECT cst_id,
+COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+-- Check unwanted spaces 
+--Expectation: No results
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+SELECT cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+SELECT * FROM silver.crm_cust_info
+
+
+
+
+-- Test the quality of crm_prd_info
+-- Checks for null or duplicate values in the primary key
+-- Expectations: No results
+SELECT
+prd_id,
+COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- Checks for unwanted spaces
+-- Expectations: No results
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+-- Check for NULL or Negative Numbers
+-- Expectations: No Results
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+-- Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+-- Check for Invalid Date Orders
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT * FROM silver.crm_prd_info
+
+
+
+
+-- Test the quality of crm_sales_details
+-- Check for Invalid date orders
+SELECT
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt 
+
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
+
+SELECT * FROM silver.crm_sales_details
+
+
+
+
+-- Test the quality of erp_cust_az12
+-- Identify out of range dates
+SELECT DISTINCT
+bdate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+gen
+FROM silver.erp_cust_az12
+
+SELECT * FROM silver.erp_cust_az12
+
+
+
+
+-- Test the quality of erp_cust_az12
+-- Data Standardization & Consistency
+SELECT DISTINCT cntry
+FROM silver.erp_loc_a101
+ORDER BY cntry
+
+SELECT * FROM silver.erp_loc_a101
+
+
+
+
+-- Test the quality of erp_px_cat_g1v2
+SELECT * FROM silver.erp_px_cat_g1v2
